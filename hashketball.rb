7a068149b195348a82d70320a93966ee14ce810e
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-03 15:13:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
    :colors =>["Black", "White"] ,
    :players => [
      {
-     :player_name => Alan , 
+     :player_name => "Alan Anderson" , 
      :number => ,
      :shoe => ,
      :points => ,
