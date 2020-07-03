9f74719a97af6ec94e9c8715c2e87f27791e3ff7
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-03 15:11:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,13 +10,12 @@ def game_hash
      :number => ,
      :shoe => ,
      :points => ,
-     => ,
-     => ,
-     => ,
-     => ,
-     => ,
-     
-   }]
+     :rebounds => ,
+     :assists => ,
+     :steals => ,
+     :blocks => ,
+     :slam_dunks => ,
+    }]
      }, 
    :away => 
    {
